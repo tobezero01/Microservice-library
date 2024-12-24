27adea9606ecfe37c1fff20d1f75f0e8c2491995
tobezero01 <ducnhuad@gmail.com>
2024-12-24 23:01:20 +0700

    first commit

---
+++ discoverServer/src/test/java/com/microservice/discoverServer/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.discoverServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
